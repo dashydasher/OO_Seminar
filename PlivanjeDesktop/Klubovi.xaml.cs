a96a495dbe7a5b61c53f85a2dd0e2ef2ffff04e4
Matea Bardun <matea.bardun@fer.hr>
2018-01-17 13:14:01 +0100

    Dodan popis klubova

--- PlivanjeDesktop/Klubovi.xaml.cs
+++ PlivanjeDesktop/Klubovi.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plivanje.Models;
+using Plivanje.Processors;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Remotion.Linq.Collections;
+using PlivanjeDesktop.ViewModels;
 
 namespace PlivanjeDesktop
 {
@@ -19,9 +23,62 @@ namespace PlivanjeDesktop
     /// </summary>
     public partial class Klubovi : Window
     {
+        private ObservableCollection<Club> clubs = new ObservableCollection<Club>();
+
         public Klubovi()
         {
             InitializeComponent();
+
+            var clubViewModel = new ClubViewModel();
+            clubViewModel.LoadClubs();
+            this.DataContext = clubViewModel;
         }
+
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            mi.IsSubmenuOpen = true;
+        }
+
+        private void MenuItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            mi.IsSubmenuOpen = false;
+        }
+
+        private void Competition_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            Natjecanja n = new Natjecanja();
+            n.Show();
+            this.Close();
+        }
+
+        private void Clubs_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            Klubovi k = new Klubovi();
+            k.Show();
+            this.Close();
+        }
+
+        private void Swimmers_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            MenuItem mi = sender as MenuItem;
+            Plivači p = new Plivači(mi.Header.ToString());
+            p.Show();
+            this.Close();
+        }
+
+        private void Records_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            MenuItem mi = sender as MenuItem;
+            Rekordi r = new Rekordi(mi.Header.ToString());
+            r.Show();
+            this.Close();
+        }
+
     }
 }
---
+++ PlivanjeDesktop/ViewModels/ClubViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Plivanje.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Remotion.Linq.Collections;
+using Plivanje.Processors;
+
+namespace PlivanjeDesktop.ViewModels
+{
+    class ClubViewModel
+    {
+        public ObservableCollection<Club> clubs { get; set; }
+        public void LoadClubs()
+        {
+            clubs = new ObservableCollection<Club>();
+            List<Club> list = new List<Club>();
+            var cp = new ClubProcessor();
+            list = cp.getClubs();
+            foreach (var club in list)
+                clubs.Add(club);
+        }
+    }
+}
