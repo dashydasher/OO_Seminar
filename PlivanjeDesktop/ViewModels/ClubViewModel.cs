6bf8477f573198383fc801a40b289cffd7444caf
Matea Bardun <matea.bardun@fer.hr>
2018-01-18 14:04:56 +0100

    Promjena mjesta  u klubovima

--- PlivanjeDesktop/ViewModels/ClubViewModel.cs
+++ PlivanjeDesktop/ViewModels/ClubViewModel.cs
@@ -15,13 +15,15 @@ namespace PlivanjeDesktop.ViewModels
             List<Club> list = new List<Club>();
             var cp = new ClubProcessor();
             list = cp.getClubs();
-            foreach (var club in list)
-                clubs.Add(club);
             //foreach (var club in list)
-            //    clubs.Add(new Club { Name = club.Name,
-            //        Address = club.Address,
-            //        Place = cp.getPlace(club.Id)
-            //    });
+            //    clubs.Add(club);
+            foreach (var club in list)
+                clubs.Add(new Club
+                {
+                    Name = club.Name,
+                    Address = club.Address,
+                    Place = cp.getPlace(club.Id)
+                });
             if (coachId!=0)
             {
                 var cr = new Plivanje.Repositories.ClubRepository();
