c1902d86284d6c926f68288ead24c08ce4105d44
Matea Bardun <matea.bardun@fer.hr>
2018-01-22 15:45:29 +0100

    Dodan procesor i repozitorij za licencu

---
+++ Plivanje/Processors/LicenceProcessor.cs
@@ -0,0 +1,30 @@
+﻿using Plivanje.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plivanje.Processors
+{
+    class LicenceProcessor
+    {
+        private ILicenceRepository _LicenceRepository;
+
+        public ILicenceRepository Repository
+        {
+            get { return _LicenceRepository; }
+            set { _LicenceRepository = value; }
+        }
+
+        public LicenceProcessor()
+        {
+            _LicenceRepository = new LicenceRepository();
+        }
+
+        public bool licenceExists(int licenceNumber)
+        {
+            return _LicenceRepository.licenceExists(licenceNumber);
+        }
+    }
+}
---
+++ Plivanje/Repositories/LicenceRepository.cs
@@ -0,0 +1,33 @@
+﻿using Plivanje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plivanje.Repositories
+{
+    public interface ILicenceRepository
+    {
+        bool licenceExists(int licenceNumber);
+
+    }
+
+    class LicenceRepository : ILicenceRepository
+    {
+        public bool licenceExists(int licenceNumber)
+        {
+            Licence result;
+            var klasa = new FluentNHibernateClass();
+            using (var session = klasa.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    result = session.QueryOver<Licence>().Where(x => x.Number == licenceNumber).List().FirstOrDefault();
+                    transaction.Commit();
+                }
+            }
+            return (result!=null);
+        }
+    }
+}
