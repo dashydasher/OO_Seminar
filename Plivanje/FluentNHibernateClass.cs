359ce9ca3d81e07998bcb796ced560d9307b603f
Petar Jalušić <petar.jalusic@fer.hr>
2018-02-04 21:01:41 +0100

    Svi class diagrami

--- Plivanje/FluentNHibernateClass.cs
+++ Plivanje/FluentNHibernateClass.cs
@@ -40,22 +40,6 @@ namespace Plivanje
             }
         }
 
-        public void TestMe()
-        {
-            var settings = ConfigurationManager.ConnectionStrings;
-
-            if (settings != null)
-            {
-                foreach (ConnectionStringSettings cs in settings)
-                {
-                    Console.WriteLine(cs.Name);
-                    Console.WriteLine(cs.ProviderName);
-                    Console.WriteLine(cs.ConnectionString);
-                }
-            }
-            //Console.WriteLine(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
-        }
-
         public ISession OpenSession()
         {
             try
